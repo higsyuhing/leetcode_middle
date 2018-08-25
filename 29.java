d10a6bb4a9a3b758a4b86bddf9ff8515075a9604
Minjun Wu <higsyuhing@users.noreply.github.com>
2018-08-24 21:59:12 -0500

    Create 29.java

---
+++ 29.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int divide(int dividend, int divisor) {
+        # copy from jiuzhangsuanfa, this sol is much more clear than others
+        # https://kingsfish.github.io/2017/10/11/Leetcode-29-Divide-Two-Integers/
+        # the exmplanation above is good
+    
+        if (divisor == 0) {
+             return dividend >= 0? Integer.MAX_VALUE : Integer.MIN_VALUE;
+        }
+        
+        if (dividend == 0) {
+            return 0;
+        }
+        
+        if (dividend == Integer.MIN_VALUE && divisor == -1) {
+            return Integer.MAX_VALUE;
+        }
+        
+        boolean isNegative = (dividend < 0 && divisor > 0) || 
+                             (dividend > 0 && divisor < 0);
+                             
+        long a = Math.abs((long)dividend);
+        long b = Math.abs((long)divisor);
+        int result = 0;
+        while(a >= b){
+            int shift = 0;
+            while(a >= (b << shift)){
+                shift++;
+            }
+            a -= b << (shift - 1);
+            result += 1 << (shift - 1);
+        }
+        return isNegative? -result: result;
+    }
+}
