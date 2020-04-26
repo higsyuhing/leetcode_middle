d5b70edf9c94654bb19df738d67525d39a03fab2
Minjun Wu <higsyuhing@users.noreply.github.com>
2020-04-25 22:10:20 -0500

    Create 490.java

---
+++ 490.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public boolean hasPath(int[][] maze, int[] start, int[] destination) {
+        
+        // in this exploration, BFS is better than DFS is quickly finding path asap instead of 
+        // exploring all the possible destinations.. 
+        
+        boolean[][] visited = new boolean[maze.length][maze[0].length];
+        int[][] dirs={{0, 1}, {0, -1}, {-1, 0}, {1, 0}};
+        Queue < int[] > queue = new LinkedList < > ();
+        queue.add(start);
+        visited[start[0]][start[1]] = true;
+        while (!queue.isEmpty()) {
+            int[] s = queue.remove();
+            if (s[0] == destination[0] && s[1] == destination[1])
+                return true;
+            for (int[] dir: dirs) {
+                int x = s[0] + dir[0];
+                int y = s[1] + dir[1];
+                while (x >= 0 && y >= 0 && x < maze.length && y < maze[0].length && maze[x][y] == 0) {
+                    x += dir[0];
+                    y += dir[1];
+                }
+                if (!visited[x - dir[0]][y - dir[1]]) {
+                    queue.add(new int[] {x - dir[0], y - dir[1]});
+                    visited[x - dir[0]][y - dir[1]] = true;
+                }
+            }
+        }
+        return false;
+    }
+}
