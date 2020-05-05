d4d90c6db63f88bc6c5755b9988ff39ef1f37d19
Minjun Wu <higsyuhing@users.noreply.github.com>
2020-05-04 21:41:27 -0500

    Create 159.java

---
+++ 159.java
@@ -0,0 +1,20 @@
+public int lengthOfLongestSubstringTwoDistinct(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+        int start = 0, end = 0;
+        int counter = 0, len = 0;
+        while(end < s.length()){
+            char cur = s.charAt(end);
+            if(!map.containsKey(cur)) map.put(cur, 0);
+            map.put(cur, map.get(cur) + 1);
+            if(map.get(cur) == 1) counter++;//new distinct char
+            while(counter > 2){//
+                char c2 = s.charAt(start);
+                map.put(c2, map.get(c2) - 1);
+                if(map.get(c2) == 0) counter--;
+                start++;
+            }
+            len = Math.max(end - start + 1, len);
+            end++;
+        }
+        return len;
+    }
