36818c5882dc8d67d1d65cf0b21de17bd8fc0d21
Minjun Wu <higsyuhing@users.noreply.github.com>
2020-05-09 17:14:39 -0500

    Create 763.py

---
+++ 763.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def partitionLabels(self, S):
+        """
+        :type S: str
+        :rtype: List[int]
+        """
+        last = {c: i for i, c in enumerate(S)}
+        j = anchor = 0
+        ans = []
+        for i, c in enumerate(S):
+            j = max(j, last[c])
+            if i == j:
+                ans.append(i - anchor + 1)
+                anchor = i + 1
+            
+        return ans
