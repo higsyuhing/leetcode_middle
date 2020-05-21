59243dce58460e536190b394ed206e36e87bbf43
Minjun Wu <higsyuhing@users.noreply.github.com>
2020-05-21 01:09:37 -0500

    Create 692.py

---
+++ 692.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def topKFrequent(self, words, k):
+        """
+        :type words: List[str]
+        :type k: int
+        :rtype: List[str]
+        """
+        # hash + pq
+        count = collections.Counter(words)
+        heap = [(-freq, word) for word, freq in count.items()]
+        heapq.heapify(heap)
+        return [heapq.heappop(heap)[1] for _ in xrange(k)]
