10961683806af6b6b15193922ba97f2c0eb86137
higsyuhing <higsyuhing@users.noreply.github.com>
2020-07-16 23:22:05 -0500

    Create 698.java

---
+++ 698.java
@@ -0,0 +1,30 @@
+enum Result { TRUE, FALSE }
+
+class Solution {
+    boolean search(int used, int todo, Result[] memo, int[] nums, int target) {
+        if (memo[used] == null) {
+            memo[used] = Result.FALSE;
+            int targ = (todo - 1) % target + 1;
+            for (int i = 0; i < nums.length; i++) {
+                if ((((used >> i) & 1) == 0) && nums[i] <= targ) {
+                    if (search(used | (1<<i), todo - nums[i], memo, nums, target)) {
+                        memo[used] = Result.TRUE;
+                        break;
+                    }
+                }
+            }
+        }
+        return memo[used] == Result.TRUE;
+    }
+    public boolean canPartitionKSubsets(int[] nums, int k) {
+        // we know the target sum-of-nums divided by k.. 
+        // DFS? put largest first, if not fit, replaced by smaller one? 
+        
+        int sum = Arrays.stream(nums).sum();
+        if (sum % k > 0) return false;
+
+        Result[] memo = new Result[1 << nums.length];
+        memo[(1 << nums.length) - 1] = Result.TRUE;
+        return search(0, sum, memo, nums, sum / k);
+    }
+}
