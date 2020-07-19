f2c4f614869af6622680ebd4c52a9a8ca4ed2ebf
higsyuhing <higsyuhing@users.noreply.github.com>
2020-07-19 08:44:01 -0500

    Create 1155.cpp

---
+++ 1155.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    /*
+    sol here, I should get that.. : 
+    https://leetcode.com/problems/number-of-dice-rolls-with-target-sum/discuss/355940/C%2B%2B-Coin-Change-2
+    */
+    int dp[31][1001] = {};
+    int numRollsToTarget(int d, int f, int target, int res = 0) {
+      if (d == 0 || target <= 0) return d == target;
+      if (dp[d][target]) return dp[d][target] - 1;
+      for (auto i = 1; i <= f; ++i)
+        res = (res + numRollsToTarget(d - 1, f, target - i)) % 1000000007;
+      dp[d][target] = res + 1;
+      return res;
+    }
+};
