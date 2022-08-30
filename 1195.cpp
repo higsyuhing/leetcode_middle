75f051eb534f606f823eadca6f4a99e8f63223b1
higsyuhing <higsyuhing@users.noreply.github.com>
2022-08-30 00:25:55 -0500

    Create 1195.cpp

---
+++ 1195.cpp
@@ -0,0 +1,35 @@
+class FizzBuzz {
+private:
+    int n;
+
+public:
+    FizzBuzz(int n) {
+        this->n = n;
+    }
+
+    // printFizz() outputs "fizz".
+    void fizz(function<void()> printFizz) {
+        
+    }
+
+    // printBuzz() outputs "buzz".
+    void buzz(function<void()> printBuzz) {
+        
+    }
+
+    // printFizzBuzz() outputs "fizzbuzz".
+	void fizzbuzz(function<void()> printFizzBuzz) {
+        
+    }
+
+    // printNumber(x) outputs "x", where x is an integer.
+    void number(function<void(int)> printNumber) {
+        
+    }
+};
+
+/*
+boring problem.. 
+
+https://leetcode.com/problems/fizz-buzz-multithreaded/discuss/387994/C%2B%2B-mutex-condition_variable-please-suggest-improvements
+*/
