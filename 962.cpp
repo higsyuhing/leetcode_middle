f909b396cf3a96b11c7ca03bb9b39dbea48ca782
higsyuhing <higsyuhing@users.noreply.github.com>
2022-12-11 17:50:06 -0600

    Create 962.cpp

---
+++ 962.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maxWidthRamp(vector<int>& nums) {
+        // use a stack, to keep decreasing values... 
+        stack<int> ms; // index of the value
+        int ret = 0; 
+
+        for (int i = 0; i < nums.size(); i++){
+            val = nums[i]; 
+            while (1){
+                if (ms.empty() || nums[ms.front()])
+            }
+
+        }
+
+
+
+
+
+    }
+};
+
+/*
+
+https://leetcode.com/problems/maximum-width-ramp/solutions/208348/java-c-python-o-n-using-stack/
+
+*/
